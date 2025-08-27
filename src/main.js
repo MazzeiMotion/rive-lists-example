84ba287492c4df7b11c1cc88af2e179c31e19ecf
Leonardo Mazzei Rossin <leomazzei.motion@gmail.com>
2025-08-27 19:53:29 -0400

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,14 +1,49 @@
 // This is the High level JS runtime for Rive
 // https://rive.app/community/doc/web-js/docvlgbnS1mp
+// Thx, Pedro!
+
+import { getBindedViewModel, computeSize } from './helpers.js';
+
+const RIVE_FILE = "button.riv";
+const DEFAULT_ARTBOARD = "default";
+const DEFAULT_STATEMACHINES = "default";
+const DEFAULT_CONTAINER = document.getElementById("canvas");
+const CONTAINER_WIDTH = '200px';
+const CONTAINER_HEIGHT = '200px';
+const DEFAULT_LAYOUT = new rive.Layout({
+    fit: rive.Fit.Contain,
+    //fit: rive.Fit.Layout, // use this for responsive layouts
+    // layoutScaleFactor: .5, // if needed
+  });
 
 const riveInstance = new rive.Rive({
-  src: "helloworld.riv",
-  canvas: document.getElementById("canvas"),
+  src: RIVE_FILE,
+  canvas: DEFAULT_CONTAINER,
   autoplay: true,
-  artboard: "Artboard",
-  stateMachines: "State Machine 1",
+  autoBind: true,
+  layout: DEFAULT_LAYOUT,
+  artboard: DEFAULT_ARTBOARD,
+  stateMachines: DEFAULT_STATEMACHINES,
 
   onLoad: () => {
-    riveInstance.resizeDrawingSurfaceToCanvas();
+    requestAnimationFrame(() => {
+      computeSize(riveInstance);
+    });
+    applyStyles(DEFAULT_CONTAINER,CONTAINER_WIDTH,CONTAINER_HEIGHT);
+    const vmi = getBindedViewModel(riveInstance); // For Data-Binds!
   },
 });
+
+function applyStyles(element, width, height) {
+  element.classList.add('canvas-modified');
+  element.style.width = width;
+  element.style.height = height;
+}
+
+// Subscribe to window size changes and update call `resizeDrawingSurfaceToCanvas`
+window.onresize = computeSize(riveInstance);
+
+// Subscribe to devicePixelRatio changes and call `resizeDrawingSurfaceToCanvas`
+window
+.matchMedia(`(resolution: ${window.devicePixelRatio}dppx)`)
+.addEventListener("change", computeSize(riveInstance));
